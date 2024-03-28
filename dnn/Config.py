d0d1422a3cd8df3db75ccfe6ae865a57db2e39e3
hyrissq <ruitian0716@gmail.com>
2024-03-29 00:31:36 +0800

    200000点；0.001学习率

--- dnn/Config.py
+++ dnn/Config.py
@@ -8,12 +8,12 @@ vertex4 = np.array([300, 150])
 margin = 0.1  # Example margin
 
 # Training para
-train_points_num = 50000
+train_points_num = 200000
 test_points_num = 128
-epoch = 80
-train_batch_size = 100
+epoch = 1000
+train_batch_size = 128
 test_batch_size = test_points_num
-learning_rate = 0.001
+learning_rate = 0.0005
 
 # oppler and w para
 fc = 6 * 10e9
