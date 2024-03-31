6cf47f79a5f370cb855994a4d36e5c93052f6453
hyrissq <ruitian0716@gmail.com>
2024-03-31 22:06:04 +0800

    RBF1.0

--- src/Config.py
+++ src/Config.py
@@ -8,17 +8,18 @@ vertex4 = np.array([300, 150])
 margin = 0.1  # Example margin
 
 # Training para
-train_points_num = 200
-test_points_num = 15
-epoch = 600
+train_points_num = 50000
+test_points_num = 100
+epoch = 50
 train_batch_size = 128
 test_batch_size = test_points_num
-learning_rate = 0.0003
+learning_rate = 0.01
+# learning_rate = 0.0003
 
 # oppler and w para
 fc = 6 * 10e9
 c = 3 * 10e8
 v = 10
 
-# using_model = "rbf"
-using_model = "dnn"
+using_model = "rbf"
+# using_model = "dnn"
--- src/RbfModel.py
+++ src/RbfModel.py
@@ -1,40 +1,94 @@
 # 定义更深的神经网络模型
+import torch
 import torch.nn as nn
-import numpy as np
-from scipy.linalg import norm, pinv
+
+
+def gaussian(alpha):
+    return torch.exp(-1 * alpha.pow(2))
 
 
 class RBF(nn.Module):
 
-    def _init_(self, input_dim, num_centers, out_dim):
-        self.input_dim = input_dim
-        self.num_centers = num_centers
-        self.out_dim = out_dim
-        self.beta = 8  # 扩展常数
-        self.centers = [
-            np.random.uniform(0, 300, input_dim) for i in range(num_centers)
-        ]
-        self.W = np.random((self.num_centers, self.out_dim))
+    def __init__(self, in_features, out_features):
+        super(RBF, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.centers = nn.Parameter(torch.Tensor(out_features, in_features))
+        self.log_sigmas = nn.Parameter(torch.Tensor(out_features))
+        self.basis_func = gaussian
+        self.reset_parameters()
 
-    def basisfunc(self, c, d):
-        return np.exp(-self.beta * norm(c - d) ** 2)
+    def reset_parameters(self):
+        # initializing centers from normal distributions with 0 mean and std 1
+        nn.init.normal_(self.centers, 0, 1)
+        # initializing sigmas with 0 value
+        nn.init.constant_(self.log_sigmas, 0)
 
-    def calcAct(self, input):
-        G = np.zeros((input.shape[0], self.num_centers), dtype=np.float)
-        for ci, c in enumerate(self.centers):
-            for xi, x in enumerate(input):
-                G[xi, ci] = self.basisfunc(c, x)
-        return G
+    def forward(self, x):
+        """Forward pass"""
+        input = x
+        size = (input.size(0), self.out_features, self.in_features)
+        x = input.unsqueeze(1).expand(size)
+        c = self.centers.unsqueeze(0).expand(size)
+        distances = (x - c).pow(2).sum(-1).pow(0.5) / torch.exp(
+            self.log_sigmas
+        ).unsqueeze(0)
+        return self.basis_func(distances)
 
-    def train(self, input, output):
-        rnd_idex = np.random.permutation(input.shape[0])[: self.num_centers]
-        self.centers = [input[i, :] for i in rnd_idex]
 
-        # 计算RBF激活函数的值
-        G = self.calcAct(input)
-        self.W = np.dot(pinv(G), output)
+class RBFNetwork(nn.Module):
+    def __init__(self):
+        in_features = 14
+        hidden_features = 10  #
+        out_features = 4
 
-    def predict(self, input):
-        G = self.calcAct(input)
-        output = np.dot(G, self.W)
-        return output
+        super(RBFNetwork, self).__init__()
+        self.rbf_layer = RBF(in_features, hidden_features)
+        self.linear_layer = nn.Linear(hidden_features, out_features)
+
+    def forward(self, x):
+        x = self.rbf_layer(x)
+        x = self.linear_layer(x)
+        return x
+
+
+# class RBF(nn.Module):
+
+#     def _init_(self):
+#         input_dim = 14
+#         out_dim = 4
+#         num_centers = 100  #
+
+#         super(RBF, self).__init__()  #
+
+#         self.input_dim = input_dim
+#         self.num_centers = num_centers
+#         self.out_dim = out_dim
+#         self.beta = 8  # 扩展常数
+#         self.centers = [
+#             np.random.uniform(0, 300, input_dim) for i in range(num_centers)
+#         ]
+#         self.W = np.random((self.num_centers, self.out_dim))
+
+#     def basisfunc(self, c, d):
+#         return np.exp(-self.beta * norm(c - d) ** 2)
+
+#     def calcAct(self, input):
+#         G = np.zeros((input.shape[0], self.num_centers), dtype=np.float)
+#         for ci, c in enumerate(self.centers):
+#             for xi, x in enumerate(input):
+#                 G[xi, ci] = self.basisfunc(c, x)
+#         return G
+
+#     def train(self, input, output):
+#         rnd_idex = np.random.permutation(input.shape[0])[: self.num_centers]
+#         self.centers = [input[i, :] for i in rnd_idex]
+
+#         # 计算RBF激活函数的值
+#         G = self.calcAct(input)
+#         self.W = np.dot(pinv(G), output)
+
+#     def predict(self, input):
+#         G = self.calcAct(input)
+#         output = np.dot(G, self.W)
+#         return output
