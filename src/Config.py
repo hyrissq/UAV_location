fc95b46725b972a21057ae2e2bcf8a273b9cd50d
danny <ruitian0716@gmail.com>
2024-04-15 00:35:24 +0800

    Updated doppler and w config

--- src/Config.py
+++ src/Config.py
@@ -10,7 +10,7 @@ test_set_num = 512
 train_step_count_per_line = 5
 length_per_step = 0.2
 angle_change_limit_per_step = 0.1
-a_b_distance = 0.05
+a_b_distance = 0.5
 
 epoch = 1000
 train_batch_size = 128
@@ -19,9 +19,9 @@ test_batch_size = test_set_num
 learning_rate = 0.0003
 
 # oppler and w para
-fc = 6 * 10e9
+fc = 6 * 10e8
 c = 3 * 10e8
-v = 10
+v = 30
 
 # using_model = "rbf"
 using_model = "dnn"
